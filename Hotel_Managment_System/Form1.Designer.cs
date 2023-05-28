5d53a3aebf31ef985dbcd1e29761360979cb915b
zeyad <zeyadmohsen1999@gmail.com>
2023-05-29 02:52:31 +0300

    zeyad

--- Hotel_Managment_System/Form1.Designer.cs
+++ Hotel_Managment_System/Form1.Designer.cs
@@ -30,23 +30,36 @@ namespace Hotel_Managment_System
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(287, 144);
+            this.label1.Location = new System.Drawing.Point(215, 117);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 17);
+            this.label1.Size = new System.Drawing.Size(29, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Kero";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(328, 103);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 52);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "zeyad";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -57,6 +70,7 @@ namespace Hotel_Managment_System
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
