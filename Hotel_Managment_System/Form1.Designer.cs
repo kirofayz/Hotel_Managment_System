1a04b9c0c27d681de02b54c52f2c16389e21a0f1
Mahmoud Esam Salem <mahmoudesamsalem95@gmail.com>
2023-05-29 03:25:55 +0300

    editted1

--- Hotel_Managment_System/Form1.Designer.cs
+++ Hotel_Managment_System/Form1.Designer.cs
@@ -37,22 +37,24 @@ namespace Hotel_Managment_System
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(251, 144);
+            this.label1.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(129, 213);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(36, 17);
+            this.label1.Size = new System.Drawing.Size(66, 28);
             this.label1.TabIndex = 0;
             this.label1.Text = "Kero";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(319, 13);
+            this.button1.Location = new System.Drawing.Point(284, 199);
             this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(140, 64);
             this.button1.TabIndex = 1;
-            this.button1.Text = "zeyad";
+            this.button1.Text = "Esam";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -68,6 +70,7 @@ namespace Hotel_Managment_System
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RoyalBlue;
             this.ClientSize = new System.Drawing.Size(700, 450);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
