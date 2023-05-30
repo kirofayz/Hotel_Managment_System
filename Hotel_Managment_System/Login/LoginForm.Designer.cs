d16eb055ac6aa9ccf0976c06270b8e6763533c92
Mahmoud Esam Salem <mahmoudesamsalem95@gmail.com>
2023-05-31 00:58:12 +0300

    Login

---
+++ Hotel_Managment_System/Login/LoginForm.Designer.cs
@@ -0,0 +1,317 @@
+﻿
+namespace Hotel_Managment_System
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.PicX = new System.Windows.Forms.PictureBox();
+            this.txtBx = new System.Windows.Forms.TextBox();
+            this.PicMini = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBoxUser = new System.Windows.Forms.TextBox();
+            this.btnLog = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBoxPass = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.PicBoxHide = new System.Windows.Forms.PictureBox();
+            this.PicBoxShow = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicX)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicMini)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PicBoxHide)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicBoxShow)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(755, 101);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(516, 338);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // PicX
+            // 
+            this.PicX.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.PicX.Image = ((System.Drawing.Image)(resources.GetObject("PicX.Image")));
+            this.PicX.Location = new System.Drawing.Point(1263, 6);
+            this.PicX.Name = "PicX";
+            this.PicX.Size = new System.Drawing.Size(91, 56);
+            this.PicX.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PicX.TabIndex = 1;
+            this.PicX.TabStop = false;
+            this.PicX.Click += new System.EventHandler(this.PicX_Click);
+            this.PicX.MouseHover += new System.EventHandler(this.PicX_MouseHover);
+            // 
+            // txtBx
+            // 
+            this.txtBx.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtBx.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtBx.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBx.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.txtBx.Location = new System.Drawing.Point(871, 445);
+            this.txtBx.Name = "txtBx";
+            this.txtBx.Size = new System.Drawing.Size(349, 28);
+            this.txtBx.TabIndex = 2;
+            this.txtBx.Text = "Hotel Management ";
+            this.txtBx.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtBx.TextChanged += new System.EventHandler(this.txtBx_TextChanged);
+            // 
+            // PicMini
+            // 
+            this.PicMini.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.PicMini.Image = ((System.Drawing.Image)(resources.GetObject("PicMini.Image")));
+            this.PicMini.Location = new System.Drawing.Point(1180, 6);
+            this.PicMini.Name = "PicMini";
+            this.PicMini.Size = new System.Drawing.Size(91, 56);
+            this.PicMini.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PicMini.TabIndex = 3;
+            this.PicMini.TabStop = false;
+            this.PicMini.Click += new System.EventHandler(this.pictureBox3_Click);
+            this.PicMini.MouseHover += new System.EventHandler(this.PicMini_MouseHover);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 547);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1366, 173);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(241, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Copyright © 2023 All Rights Reserved";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtBoxPass);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.btnLog);
+            this.groupBox1.Controls.Add(this.txtBoxUser);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.PicBoxShow);
+            this.groupBox1.Controls.Add(this.PicBoxHide);
+            this.groupBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.groupBox1.Location = new System.Drawing.Point(38, 84);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(496, 389);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Please Login First";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(87, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Username :";
+            // 
+            // txtBoxUser
+            // 
+            this.txtBoxUser.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtBoxUser.Location = new System.Drawing.Point(72, 103);
+            this.txtBoxUser.Name = "txtBoxUser";
+            this.txtBoxUser.Size = new System.Drawing.Size(276, 24);
+            this.txtBoxUser.TabIndex = 1;
+            // 
+            // btnLog
+            // 
+            this.btnLog.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnLog.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnLog.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnLog.Location = new System.Drawing.Point(90, 294);
+            this.btnLog.Name = "btnLog";
+            this.btnLog.Size = new System.Drawing.Size(250, 36);
+            this.btnLog.TabIndex = 3;
+            this.btnLog.Text = "Log in";
+            this.btnLog.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(89, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Password :";
+            // 
+            // txtBoxPass
+            // 
+            this.txtBoxPass.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtBoxPass.Location = new System.Drawing.Point(72, 176);
+            this.txtBoxPass.Name = "txtBoxPass";
+            this.txtBoxPass.Size = new System.Drawing.Size(276, 24);
+            this.txtBoxPass.TabIndex = 2;
+            this.txtBoxPass.UseSystemPasswordChar = true;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label4.AutoSize = true;
+            this.label4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label4.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.label4.Location = new System.Drawing.Point(160, 252);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(121, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Forget Password ?";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // PicBoxHide
+            // 
+            this.PicBoxHide.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PicBoxHide.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.PicBoxHide.Image = ((System.Drawing.Image)(resources.GetObject("PicBoxHide.Image")));
+            this.PicBoxHide.Location = new System.Drawing.Point(344, 176);
+            this.PicBoxHide.Name = "PicBoxHide";
+            this.PicBoxHide.Size = new System.Drawing.Size(51, 24);
+            this.PicBoxHide.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PicBoxHide.TabIndex = 6;
+            this.PicBoxHide.TabStop = false;
+            this.PicBoxHide.Click += new System.EventHandler(this.PicBoxHide_Click);
+            // 
+            // PicBoxShow
+            // 
+            this.PicBoxShow.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PicBoxShow.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.PicBoxShow.Image = ((System.Drawing.Image)(resources.GetObject("PicBoxShow.Image")));
+            this.PicBoxShow.Location = new System.Drawing.Point(341, 176);
+            this.PicBoxShow.Name = "PicBoxShow";
+            this.PicBoxShow.Size = new System.Drawing.Size(54, 24);
+            this.PicBoxShow.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PicBoxShow.TabIndex = 7;
+            this.PicBoxShow.TabStop = false;
+            this.PicBoxShow.Click += new System.EventHandler(this.PicBoxShow_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel2.BackColor = System.Drawing.Color.LightGray;
+            this.panel2.Location = new System.Drawing.Point(653, 72);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(5, 435);
+            this.panel2.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.textBox1.Location = new System.Drawing.Point(860, 479);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(349, 28);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "System";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(1366, 720);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.txtBx);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.PicMini);
+            this.Controls.Add(this.PicX);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicX)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicMini)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PicBoxHide)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicBoxShow)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox PicX;
+        private System.Windows.Forms.TextBox txtBx;
+        private System.Windows.Forms.PictureBox PicMini;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtBoxPass;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnLog;
+        private System.Windows.Forms.TextBox txtBoxUser;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox PicBoxShow;
+        private System.Windows.Forms.PictureBox PicBoxHide;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
